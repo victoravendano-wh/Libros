eaa32294329bf1d4bcb974b6581dc905d0e1ac36
victor-wisphub <victor_armando@wisphub.net>
2026-08-20 14:26:29 -0500

    fecha actual usando html templates

--- libros/views.py
+++ libros/views.py
@@ -1,15 +1,17 @@
 from django.http import HttpResponse, Http404
 import datetime
-
+from django.template import Context
+from django.template.loader import get_template
+from django.shortcuts import render
 
 # def hola(request):
 #     return HttpResponse("hola mundo")
 
 def fecha_actual(request):
     ahora = datetime.datetime.now()
-    html = "<html><body><h1>Fecha</h1><h3>%s</h3></body></html>"%ahora
+    t = get_template('fecha_actual.html')
+    html = t.render(Context({'fecha_actual':ahora}))
     return HttpResponse(html)
-
 
 def horas_adelante(request, offset):
     try:
