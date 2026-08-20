8e498754e5f79fea855c54cf53d81ce066cb0c08
victor-wisphub <victor_armando@wisphub.net>
2026-08-20 16:55:19 -0500

    Se crea la primer app, validamos modelos mediante funciones y creamos el primer template dinamico

--- libros/views.py
+++ libros/views.py
@@ -3,21 +3,24 @@ import datetime
 from django.template import Context
 from django.template.loader import get_template
 from django.shortcuts import render
+import psycopg2
 
 # def hola(request):
 #     return HttpResponse("hola mundo")
 
 def fecha_actual(request):
     ahora = datetime.datetime.now()
-    t = get_template('fecha_actual.html')
-    html = t.render(Context({'fecha_actual':ahora}))
-    return HttpResponse(html)
+    return render(request, 'fecha_actual.html', {"fecha_actual":ahora})
+
 
 def horas_adelante(request, offset):
     try:
         offset = int(offset)
     except:
         raise Http404()
-    hora = datetime.datetime.now()+datetime.timedelta(hours=offset)
-    html = "<html><body><h1>Fecha</h1><h3>En %s horas serian %s</h3></body></html>"%(offset,hora)
-    return HttpResponse(html)
+    horas = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    return render(request, "fecha_horas_adelante.html", {'horas':offset, 'hora_siguiente':horas})
+
+def lista_biblioteca(request):
+    db = psycopg2.connect(database='librosdb',user='victor',password='1325164',host='localhost')
+    
