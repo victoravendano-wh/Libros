d700d207d09330859958ddca6151158964747db7
victor-wisphub <victor_armando@wisphub.net>
2026-08-21 12:59:08 -0500

    Finalizamos capitulo 6 del djangolibro, con el tema del panel administrativo

--- apps/biblioteca/admin.py
+++ apps/biblioteca/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from apps.biblioteca.models import Editor, Autor, Libro
+
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellidos', 'email')
+    search_fields = ('nombre', 'apellidos')
+    
+class LibroAdmin(admin.ModelAdmin):
+    list_display= ('titulo', 'editor', 'fecha_publicada')
+    list_filter = ('fecha_publicada',)
+    date_hierarchy = 'fecha_publicada'
+    ordering = ('-fecha_publicada',)
+    fields = ('titulo', 'autores', 'editor', 'portada')
+    filter_horizontal = ("autores",)
+    raw_id_fields = ('editor',)
+
+admin.site.register(Editor)
+admin.site.register(Autor, AutorAdmin)
+admin.site.register(Libro, LibroAdmin)
--- apps/biblioteca/models.py
+++ apps/biblioteca/models.py
@@ -9,26 +9,24 @@ class Editor(models.Model):
     ciudad = models.CharField(max_length=50)
     estado = models.CharField(max_length=50)
     pais = models.CharField(max_length=50)
-    website = models.URLField()
+    website = models.URLField(blank=True)
     def __str__(self):
         return self.nombre
 
 
-
-
-
 class Autor(models.Model):
     nombre = models.CharField(max_length=60)
-    apelildos = models.CharField(max_length=50)
-    email = models.EmailField()
+    apellidos = models.CharField(max_length=50)
+    email = models.EmailField(blank=True, verbose_name="E-mail")
     def __str__(self):
         return self.nombre
     
 class Libro(models.Model):
     titulo = models.CharField(max_length=50)
     autores = models.ManyToManyField(Autor)
-    editor = models.ForeignKey(Editor)
-    fecha_publicada = models.DateField()
-    portada = models.ImageField(upload_to='portadas')
+    editor = models.ForeignKey(Editor,  on_delete=models.CASCADE)
+    fecha_publicada = models.DateField(null=True, blank=True)
+    portada = models.ImageField(upload_to='portadas', blank=True)
     def __str__(self):
         return self.titulo
+    
